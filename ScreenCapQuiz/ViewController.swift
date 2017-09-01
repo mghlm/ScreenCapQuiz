299172b381c80203435abec6bf775b4d45e11936
Magnus Holm <mholm@PEG-MAC-009556.local>
2017-09-01 14:49:40 +0100

    Initial Commit

---
+++ ScreenCapQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenCapQuiz
+//
+//  Created by Magnus Holm on 01/09/2017.
+//  Copyright © 2017 Magnus Holm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
