5857f7832fae607a5fbbcbd9789e9078232af257
Magnus Holm <mail@magnusholm.com>
2017-09-04 10:17:28 +0100

    Add MoviesScreen class

---
+++ MoviesScreen.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesScreen.swift
+//  ScreenCapQuiz
+//
+//  Created by Magnus Holm on 04/09/2017.
+//  Copyright © 2017 Magnus Holm. All rights reserved.
+//
+
+import Foundation
+
+class MoviesScreen {
+    
+    let movie1: Movie
+    let movie2: Movie
+    let movie3: Movie
+    let movie4: Movie
+    
+    init(movie1: Movie, movie2: Movie, movie3: Movie, movie4: Movie) {
+        self.movie1 = movie1
+        self.movie2 = movie2
+        self.movie3 = movie3
+        self.movie4 = movie4
+    }
+}
